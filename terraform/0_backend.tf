556cb0970e74b81bc21e08601fb0cb7ff62c76a2
Carlos Solis <casolis@wrbrekley.com>
2026-07-13 19:08:30 -0500

    Update backend configuration with resource group and storage account names

--- terraform/0_backend.tf
+++ terraform/0_backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.15.0"
 
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
+    resource_group_name  = "rg-utpdavid2026-GEN-3p"
+    storage_account_name = "stgutpdavid2026gen3p"
     container_name       = "tfstate-user1"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
